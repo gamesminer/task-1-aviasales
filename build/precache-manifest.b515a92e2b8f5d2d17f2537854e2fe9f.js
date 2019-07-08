0ddbcf2491421014fc7049efeed52157253afde2
Ivan Yevsiutin <iyevsiut@cisco.com>
2019-07-09 00:30:23 +0300

    Hide official information from firebase

---
+++ build/precache-manifest.b515a92e2b8f5d2d17f2537854e2fe9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "657b2b98641b1501adb40d97be3e2999",
+    "url": "/task-1-aviasales/index.html"
+  },
+  {
+    "revision": "82b1817ca71bbb74bc53",
+    "url": "/task-1-aviasales/static/css/main.2c166950.chunk.css"
+  },
+  {
+    "revision": "d074a79e1e4c7a4a795d",
+    "url": "/task-1-aviasales/static/js/2.74c720d3.chunk.js"
+  },
+  {
+    "revision": "82b1817ca71bbb74bc53",
+    "url": "/task-1-aviasales/static/js/main.b7a737ff.chunk.js"
+  },
+  {
+    "revision": "88d5ef1a2b4e7ecbd64f",
+    "url": "/task-1-aviasales/static/js/runtime~main.26b6f5d0.js"
+  },
+  {
+    "revision": "7ea5a3b3ea2e641b93495d6a6c348d9c",
+    "url": "/task-1-aviasales/static/media/plane.7ea5a3b3.png"
+  },
+  {
+    "revision": "fc29347352d33a62d5f3a670bbc234a3",
+    "url": "/task-1-aviasales/static/media/turkish.fc293473.png"
+  }
+]);
--- src/services/FirebaseService.js
+++ src/services/FirebaseService.js
@@ -4,13 +4,13 @@ import "firebase/storage";
 import "firebase/auth";
 
 const config = {
-  apiKey: "AIzaSyArforO5vSGylgGO4BK0vkxtuT1Mu5Sjck",
-  authDomain: "tz-aviasales.firebaseapp.com",
-  databaseURL: "https://tz-aviasales.firebaseio.com",
-  projectId: "tz-aviasales",
-  storageBucket: "",
-  messagingSenderId: "322434351956",
-  appId: "1:322434351956:web:1ea8b738c50d2c88"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 export const FirebaseService = initializeApp(config);
